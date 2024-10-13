f12c405f91e04457f46acdfc224dfd890adbdb56
Talha Ahmed <t01haa2005@gmail.com>
2024-10-13 16:49:07 -0700

    copy paste given code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,47 @@
 #include <iostream>
 using namespace std;
 
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+
+class DoublyLinkedList {
+private:
+    struct Node {
+        int data;
+        Node* prev;
+        Node* next;
+        Node(int val, Node* p = nullptr, Node* n = nullptr) {
+            data = val;
+            prev = p;
+            next = n;
+        }
+    };
+    Node* head;
+    Node* tail;
+
+public:
+    // Constructor
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    // Insert at the back
+    void push_back(int value) {
+        Node* newNode = new Node(value);
+        if (!tail) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    // Insert at the front
+    void push_front(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    
